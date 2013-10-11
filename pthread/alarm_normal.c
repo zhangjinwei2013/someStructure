8b7f747159eb65699ef0ae3d20f19fb2b0916ffd
zhangjinwei2013 <zjw890924@163.com>
2013-10-11 23:18:36 +0800

    add

---
+++ pthread/alarm_normal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 512
+
+int main()
+{
+    char line[SIZE] = {0};
+    int seconds;
+    char msg[SIZE];
+    while (true)
+    {
+        if (fgets(line,sizeof(line),stdin) == NULL)
+            return -1;
+        if (strlen(line) <= 1)
+            continue;
+        if (sscanf(line,"%d %64[^\n]",&seconds,msg) < 2)
+        {       
+            printf("sorry,bad command\n");
+            return -1;
+        }
+        else
+        {
+            printf("msg,%s\n",msg);
+            sleep(seconds);
+        }
+    }
+}
